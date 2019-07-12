9fb1e678485680f489d72109c33f0e89e10965b8
n0vad3v <n0vad3v@riseup.net>
2019-07-12 23:36:08 +0800

    Add basic tests

--- app/search/tests.py
+++ app/search/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+import pytest
 
+import search.schema
 # Create your tests here.
+def test_basic_search():
+    # Search for 'le'
+    query = """
+    query interviewCardSuggestions ($query: String){
+      company(query:"le"){
+        id
+        name
+      }
+    }
+    """
+    expected = {
+      "data": {
+        "company": [
+          {
+            "id": "1",
+            "name": "力扣（LeetCode）"
+          },
+          {
+            "id": "6",
+            "name": "咕果科技"
+          }
+        ]
+      }
+    }
+
+    result = schema.excute(query)
+    assert not result.errors
+    assert result.data == expected
