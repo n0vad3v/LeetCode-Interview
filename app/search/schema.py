0f2b3b3546525a001b5a300b726e042fc7b925af
n0vad3v <n0vad3v@riseup.net>
2019-07-13 19:04:35 +0800

    Add redis cache on resolve companies, Update README

--- app/search/schema.py
+++ app/search/schema.py
@@ -8,6 +8,7 @@ from .tasks import *
 from django.conf import settings
 from django.views.decorators.cache import cache_page
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.core.cache import cache
 
 # For getting statistic year-month-week_of_month
 import numpy as np
@@ -30,7 +31,7 @@ class CompanyNode(DjangoObjectType):
 
 class Query(object):
     company = graphene.List(CompanyNode,
-                             query=graphene.String())
+                             query=graphene.String(required=True))
     hot_search_companies = graphene.List(CompanyNode,
                             week=graphene.String(required=True),
                             top_count=graphene.Int())
@@ -39,11 +40,24 @@ class Query(object):
     def resolve_company(self,info,**kwargs):
         query = kwargs.get('query')
 
+        cache_name = "resolve_company_"+query
+
         if query is not None:
-            companies = Company.objects.filter(Q(name__contains = query) | Q(slug__contains = query) | Q(pinyin__contains = query))
+            if cache_name in cache:
+                companies = cache.get_many([cache_name])
+                companies = companies[cache_name]
+                print("from cache")
+            else:
+                companies = Company.objects.filter(Q(name__contains = query) | Q(slug__contains = query) | Q(pinyin__contains = query))
+                print("not from cache")
+                cache.set(cache_name,companies)
+
+            # Search Hit increment
+            company_ids = []
             for company in companies:
-                update_search_hit_count_by_id.delay(company.id,get_current_week_of_month())
-            # Add hit_count
+                company_ids.append(company.id)
+            update_search_hit_count.delay(company_ids,get_current_week_of_month())
+
             return companies
 
         return None
@@ -52,14 +66,23 @@ class Query(object):
         week = kwargs.get('week')
         top_count = kwargs.get('top_count')
 
+        cache_name = "hot_search_"+week+"_"+"top_count"
+
         if week is not None:
-            top_companies = Search.objects.order_by('-search_hit').filter(week=week)[:top_count]
+            if cache_name in cache:
+                print("from cache")
+                top_companies = cache.get_many([cache_name])
+                top_companies = top_companies[cache_name]
+            else:
+                print("not from cache")
+                top_companies = Search.objects.order_by('-search_hit').filter(week=week)[:top_count]
+                cache.set(cache_name,top_companies)
         else:
             top_companies = Search.objects.order_by('-search_hit').all()[:top_count]
 
         companies_with_search_hit = []
         for company_search in top_companies:
-            company = company_search.slug
+            company = company_search.company_id
             companies_with_search_hit.append(company)
         return companies_with_search_hit
     
--- app/search/tasks.py
+++ app/search/tasks.py
@@ -4,12 +4,13 @@ from celery.decorators import task
 
 from .models import Search,Company
 
-@task(name='search.tasks.update_search_hit_count_by_id')
-def update_search_hit_count_by_id(company_id,query_week):
-    company = Company.objects.get(pk=company_id)
-    print(company.name)
-    search_log_row = Search.objects.get(slug__id=company_id,week=query_week)
-    print(search_log_row.search_hit)
-    new_search_hit = search_log_row.search_hit + 1
-    search_log_row.search_hit = new_search_hit
-    search_log_row.save()
+@task(name='search.tasks.update_search_hit_count')
+def update_search_hit_count(company_ids,query_week):
+    for company_id in company_ids:
+        company = Company.objects.get(pk=company_id)
+        print(company.name)
+        search_log_row = Search.objects.get(company_id__id=company_id,week=query_week)
+        print(search_log_row.search_hit)
+        new_search_hit = search_log_row.search_hit + 1
+        search_log_row.search_hit = new_search_hit
+        search_log_row.save()
