ceff0b12465a3a0a75a2c6a293da59942f38135e
n0vad3v <n0vad3v@riseup.net>
2019-07-15 13:53:43 +0800

    Bug fix, create current week data if not exists.

--- app/search/tasks.py
+++ app/search/tasks.py
@@ -9,8 +9,14 @@ def update_search_hit_count(company_ids,query_week):
     for company_id in company_ids:
         company = Company.objects.get(pk=company_id)
         print(company.name)
+        print(company_id)
         # TODO: Optimize with bulk update: https://docs.djangoproject.com/en/2.1/topics/db/queries/#updating-multiple-objects-at-once
-        search_log_row = Search.objects.get(company_id__id=company_id,week=query_week)
+        try:
+            search_log_row = Search.objects.get(company_id__id=company_id,week=query_week)
+        except:
+            print("Not exist,creating")
+            search_lot_row = Search.objects.create(company_id = company,week=query_week,search_hit=1)
+            search_log_row = Search.objects.get(company_id__id=company_id,week=query_week)
         new_search_hit = search_log_row.search_hit + 1
         search_log_row.search_hit = new_search_hit
         search_log_row.save()
